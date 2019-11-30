1d0cf68b8d4a9dc3c70933733694e823e99d1669
Huang Grace <huang.yahui.grace@gmail.com>
2019-11-30 15:27:07 +0800

    change each row's image

--- HelloCollectionView/ViewController.swift
+++ HelloCollectionView/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
-    
+    var itemsInLine:CGFloat = 3
     var images:[UIImage?] = []
     
     @IBOutlet weak var myCollectionView: UICollectionView!
@@ -41,6 +41,12 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
                                                selector: #selector(self.rotate),
                                                name: UIDevice.orientationDidChangeNotification,
                                                object: nil)
+//        NotificationCenter.default.addObserver(forName: UIDevice.orientationDidChangeNotification,
+//            object: self,
+//            queue: OperationQueue.main {
+//                sizefunc(true)
+//            },
+//            using: <#T##(Notification) -> Void#>)
     }
     
     func sizefunc(_ animated:Bool) {
@@ -48,7 +54,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 6, left: 6, bottom: 6, right: 6)
         layout.minimumLineSpacing = 10
-        let imageSize = screenSize.width / 3 - 12
+        let imageSize = screenSize.width / itemsInLine - 12
         layout.itemSize = CGSize(width: imageSize, height: imageSize)
         myCollectionView.setCollectionViewLayout(layout, animated: animated)
     }
@@ -57,7 +63,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         print("rotate")
         sizefunc(true)
     }
-
+    
+    //MARK: CollectionView Delegate
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
@@ -69,6 +76,23 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         //cell.backgroundColor = UIColor.yellow
         cell.myImage.image = images[indexPath.row]
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
+    }
+    
+    @IBAction func action2(_ sender: Any) {
+        itemsInLine = 2
+        rotate()
+    }
+    @IBAction func action3(_ sender: Any) {
+        itemsInLine = 3
+        rotate()
+    }
+    @IBAction func action5(_ sender: Any) {
+        itemsInLine = 4
+        rotate()
     }
 }
 
